66cabecfb08c400440dde9d586d927c86b30e0ca
egbodofo <femi.egbodofo@trilogy.com>
2022-07-12 10:48:19 +0100

    added state to input component

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './InputComponent.css';
- 
-const InputComponent = () => {
+import { useState } from "react";
+  
+export default function InputComponent() {
+  const [text, setText] = useState("");
+
+  const getInput = (t) => {
+    console.log(t);
+    setText(t);
+  };
+
   return (
-    <div className="input-container">
-        <p className='input-header'>Note: You can only input a "falsy value", an "array" or "anything else"</p>
-        <input className='input-body' type="text"></input>
+    <div className="input-parent-container">
+      <Component getInput={getInput} value={text} />
+      <p className='input-text-output'>{text}</p>
     </div>
   );
 }
- 
-export default InputComponent;
+
+const Component = ({ getInput, value }) => {
+  return (
+    <>
+    <div className='input-child-container'>
+      <h1 className='input-child-header'> Type in the box bellow</h1>
+      <input className='input-child-box' type="text" value={value} onChange={(e) => getInput(e.target.value)}></input>
+    </div>
+    </>
+  );
+};
